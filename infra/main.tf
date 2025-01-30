2efb2f7dd56d0081808735213dc6d0beb662fa3f
gaava <nero.gava@gmail.com>
2025-01-30 18:05:35 -0300

    feat: fix variables input

--- infra/main.tf
+++ infra/main.tf
@@ -90,6 +90,7 @@ resource "aws_lb_target_group" "frameshot_tg" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
 
   health_check {
     path                = "/healthcheck"
